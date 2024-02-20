1f34a447cfca0ad5e1d22f3b7842342f9830c279
inpursuit22 <114816005+inpursuit22@users.noreply.github.com>
2024-02-20 16:22:04 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,64 @@ variable "private_google_access" {
   type = bool
   default = true
 }
+
+variable "firewall-ports" {
+  type = list
+  default = ["80", "8080", "1000-2000", "22"]
+}
+
+variable "compute-source-tags" {
+    type = list
+    default = ["web"]
+}
+
+variable "target_environment" {
+  default = "DEV"
+}
+
+variable "environment_list" {
+  type = list(string)
+  default = ["DEV","QA","STAGE","PROD"]
+}
+
+variable "environment_map" {
+  type = map(string)
+  default = {
+    "DEV" = "dev",
+    "QA" = "qa",
+    "STAGE" = "stage",
+    "PROD" = "prod"
+  }
+}
+
+variable "environment_machine_type" {
+  type = map(string)
+  default = {
+    "DEV" = "f1-micro",
+    "QA" = "f1-micro",
+    "STAGE" = "f1-micro",
+    "PROD" = "f1-micro"
+  }
+}
+
+variable "environment_instance_settings" {
+  type = map(object({machine_type=string, tags=list(string)}))
+  default = {
+    "DEV" = {
+      machine_type = "f1-micro"
+      tags = ["dev"]
+    },
+   "QA" = {
+      machine_type = "f1-micro"
+      tags = ["qa"]
+    },
+    "STAGE" = {
+      machine_type = "f1-micro"
+      tags = ["stage"]
+    },
+    "PROD" = {
+      machine_type = "f1-micro"
+      tags = ["prod"]
+    }
+  }
+}
